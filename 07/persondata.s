fd39b3673a89ca46bbb318ae50542ba629cd8ec3
noahmarro <naoriglo@gmail.com>
2023-11-05 22:42:46 +0100

    07 persondata

--- 07/persondata.s
+++ 07/persondata.s
@@ -1,11 +1,11 @@
 .section .data
 
+# Array of people
 .globl numpeople, people
 numpeople:
-  # Calculate the number of people in the people array
+  # Calculate the number of elements in the array
   .quad (endpeople - people)/PERSON_RECORD_SIZE
 people:
-  # Array of people
   .quad 200, 2, 74, 20
   .quad 135, 4, 65, 21 # Me!
   .quad 150, 1, 68, 30
@@ -14,13 +14,13 @@ people:
   .quad 180, 5, 69, 65
 endpeople:
 
-# Total size of the struct
-.globl PERSON_RECORD_SIZE 
-.equ PERSON_RECORD_SIZE, 32
-
 # Describe the components of the struct
 .globl WEIGHT_OFFSET, HAIR_OFFSET, HEIGHT_OFFSET, AGE_OFFSET
 .equ WEIGHT_OFFSET, 0
 .equ HAIR_OFFSET, 8
 .equ HEIGHT_OFFSET, 16
 .equ AGE_OFFSET, 24
+
+# Total size of the struct
+.globl PERSON_RECORD_SIZE 
+.equ PERSON_RECORD_SIZE, 32
